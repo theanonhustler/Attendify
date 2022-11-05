60a821415c3fdf18e18cd21317c68853c29cf859
anonhustler <theanonhustler@gmail.com>
2022-11-05 01:45:23 +0100

    finish event and recent event component

--- src/common/Button/index.tsx
+++ src/common/Button/index.tsx
@@ -4,10 +4,15 @@ import Link from "next/link";
 type ButtonProps = {
   label: string;
   className: string;
+  onClick?: () => void;
 };
-function Button({ label, className, ...props }: ButtonProps) {
+function Button({ label, className, onClick, ...props }: ButtonProps) {
   return (
-    <button className={`outline-none border-opacity-30 p-4 rounded-lg ${className}`}  {...props}>
+    <button
+      onClick={onClick}
+      className={`outline-none border-opacity-30 p-4 rounded-lg ${className}`}
+      {...props}
+    >
       {label}
     </button>
   );
---
+++ src/pages/recentEvent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LayOut from "@layout/LayOut";
+import Recent from "@components/recent/Recent";
+
+function upcomingEvents() {
+  return (
+    <LayOut>
+      <Recent />
+    </LayOut>
+  );
+}
+
+export default upcomingEvents;
