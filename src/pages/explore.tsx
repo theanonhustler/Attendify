996e3b2e2cf4c9d84281eda5eb9f3131508b5314
anonhustler <theanonhustler@gmail.com>
2022-11-05 19:00:14 +0100

    ft: explore component completed

---
+++ src/pages/explore.tsx
@@ -0,0 +1,13 @@
+import Explore from "@components/explore/explore";
+import LayOut from "@layout/LayOut";
+import React from "react";
+
+function ExploreEvent() {
+  return (
+    <LayOut>
+      <Explore />
+    </LayOut>
+  );
+}
+
+export default ExploreEvent;
