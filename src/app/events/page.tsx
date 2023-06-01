f3008cd6629c5e06ee2e084a3739803ada593a30
anonhustler <theanonhustler@gmail.com>
2023-06-01 14:41:58 +0100

    explore page complete

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import people from "@public/assets/people.png";
-import hacker from "@public/assets/hacker.svg";
+import { collectionItem } from "static/data";
+import Collection from "@components/collection/Collection";
 
 const Events = () => {
   return (
@@ -34,7 +35,7 @@ const Events = () => {
           </p>
           <Link
             href="/connect"
-            className="bg-[#6E4AE7] text-[#F9F8FB] border border-solid border-purple-400 font-normal text-base leading-6 px-3 py-2 rounded-lg h-[20%] md:w-[30%] w-[80%] text-center"
+            className="bg-[#6E4AE7] text-[#F9F8FB] border border-solid border-purple-400 font-normal text-sm leading-6 px-2 py-2 rounded-lg h-[20%] md:w-[30%] w-[80%] text-center"
           >
             contact us
           </Link>
@@ -46,127 +47,10 @@ const Events = () => {
       <h2 className="font-syne font-normal text-2xl leading-10 text-[#F8F9FB] py-2">
         Recent Events
       </h2>
-      <div className="grid gap-8 md:grid-cols-2 md:gap-8 lg:grid-cols-4 lg:gap-12">
-        <div className=" border border-[#3D33A9] p-2 relative rounded-lg md:border-0 md:p-0  md:block md:relative lg:border-0 lg:block lg:relative lg:p-0 bg-card">
-          <div>
-            <Image src={hacker} className="w-full" alt="hacker image" />
-          </div>
-          <div className="w-full md:border-b md:border-r md:border-l md:border-[#3D33A9] md:rounded-b-lg lg:border-b lg:border-r lg:border-l lg:border-[#3D33A9] lg:rounded-b-lg">
-            <div className="p-4 relative">
-              <div>
-                <span className="font-minimal leading-sm-xx text-[#9D94B8] font-jakarta">
-                  Name of Event
-                </span>
-                <h2 className="font-jakarta font-minimal leading-sm-xx font-semibold mb-1 text-[#F9F8FB]">
-                  Attendify Launch Party <br />
-                  <span className="font-minimal leading-sm-xx text-[#9D94B8] font-jakarta">
-                    Organisers
-                  </span>
-                  <p className="text-[#F9F8FB] font-jakarta font-semibold my-2">
-                    {" "}
-                    Attendify
-                  </p>
-                </h2>
-                <span className="my-2 font-minimal leading-sm-xx text-[#9D94B8] font-jakarta">
-                  Date
-                </span>
-                <p className="font-jakarta  text-[#F9F8FB]  mb-1">
-                  October,2022
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className=" border border-[#3D33A9] p-2 relative rounded-lg md:border-0 md:p-0  md:block md:relative lg:border-0 lg:block lg:relative lg:p-0 bg-card">
-          <div>
-            <Image src={hacker} className="w-full" alt="hacker image" />{" "}
-          </div>
-          <div className="w-full md:border-b md:border-r md:border-l md:border-[#3D33A9] md:rounded-b-lg lg:border-b lg:border-r lg:border-l lg:border-[#3D33A9] lg:rounded-b-lg">
-            <div className="p-4 relative">
-              <div>
-                <span className="font-minimal leading-sm-xx text-[#9D94B8] font-jakarta">
-                  Name of Event
-                </span>
-                <h2 className="font-jakarta font-minimal leading-sm-xx font-semibold mb-1 text-[#F9F8FB]">
-                  Attendify Launch Party <br />
-                  <span className="font-minimal leading-sm-xx text-[#9D94B8] font-jakarta">
-                    Organisers
-                  </span>
-                  <p className="text-[#F9F8FB] font-jakarta font-semibold my-2">
-                    {" "}
-                    Attendify
-                  </p>
-                </h2>
-                <span className="my-2 font-minimal leading-sm-xx text-[#9D94B8] font-jakarta">
-                  Date
-                </span>
-                <p className="font-jakarta  text-[#F9F8FB]  mb-1">
-                  October,2022
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className=" border border-[#3D33A9] p-2 relative rounded-lg md:border-0 md:p-0  md:block md:relative lg:border-0 lg:block lg:relative lg:p-0 bg-card">
-          <div>
-            <Image src={hacker} className="w-full" alt="hacker image" />{" "}
-          </div>
-          <div className="w-full md:border-b md:border-r md:border-l md:border-[#3D33A9] md:rounded-b-lg lg:border-b lg:border-r lg:border-l lg:border-[#3D33A9] lg:rounded-b-lg">
-            <div className="p-4 relative">
-              <div>
-                <span className="font-minimal leading-sm-xx text-[#9D94B8] font-jakarta">
-                  Name of Event
-                </span>
-                <h2 className="font-jakarta font-minimal leading-sm-xx font-semibold mb-1 text-[#F9F8FB]">
-                  Attendify Launch Party <br />
-                  <span className="font-minimal leading-sm-xx text-[#9D94B8] font-jakarta">
-                    Organisers
-                  </span>
-                  <p className="text-[#F9F8FB] font-jakarta font-semibold my-2">
-                    {" "}
-                    Attendify
-                  </p>
-                </h2>
-                <span className="my-2 font-minimal leading-sm-xx text-[#9D94B8] font-jakarta">
-                  Date
-                </span>
-                <p className="font-jakarta  text-[#F9F8FB]  mb-1">
-                  October,2022
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className=" border border-[#3D33A9] p-2 relative rounded-lg md:border-0 md:p-0  md:block md:relative lg:border-0 lg:block lg:relative lg:p-0 bg-card">
-          <div>
-            <Image src={hacker} className="w-full" alt="hacker image" />{" "}
-          </div>
-          <div className="w-full md:border-b md:border-r md:border-l md:border-[#3D33A9] md:rounded-b-lg lg:border-b lg:border-r lg:border-l lg:border-[#3D33A9] lg:rounded-b-lg">
-            <div className="p-4 relative">
-              <div>
-                <span className="font-minimal leading-sm-xx text-[#9D94B8] font-jakarta">
-                  Name of Event
-                </span>
-                <h2 className="font-jakarta font-minimal leading-sm-xx font-semibold mb-1 text-[#F9F8FB]">
-                  Attendify Launch Party <br />
-                  <span className="font-minimal leading-sm-xx text-[#9D94B8] font-jakarta">
-                    Organisers
-                  </span>
-                  <p className="text-[#F9F8FB] font-jakarta font-semibold my-2">
-                    {" "}
-                    Attendify
-                  </p>
-                </h2>
-                <span className="my-2 font-minimal leading-sm-xx text-[#9D94B8] font-jakarta">
-                  Date
-                </span>
-                <p className="font-jakarta  text-[#F9F8FB]  mb-1">
-                  October,2022
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="grid gap-8 grid-cols-2 md:grid-cols-4 md:gap-12">
+        {collectionItem.map((collection, index) => (
+          <Collection key={index} {...collection} />
+        ))}
       </div>
     </div>
   );
